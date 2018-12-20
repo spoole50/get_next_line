fd4213965b1154fbc34094785bf967d936d8b948
Shane Poole <spoole@e1z2r5p29.42.us.org>
2018-12-20 15:12:44 -0800

    rough working

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,13 @@
 /*   By: spoole <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/17 17:34:07 by spoole            #+#    #+#             */
-/*   Updated: 2018/12/20 12:25:35 by spoole           ###   ########.fr       */
+/*   Updated: 2018/12/20 15:10:49 by spoole           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char 	*ft_gnl_strjoin(char *s1)
+char	*ft_gnl_strjoin(char *s1)
 {
 	char	*result;
 	int		i;
@@ -44,35 +44,54 @@ int		nextline(char *str)
 	return (0);
 }
 
-int		get_next_line(const int fd, char **line)
+char	*init_temp(char *temp, int *i)
 {
-	static char	*temp;
-	int			check;
-	int			x;
-
-	check = 0;
 	if (temp != NULL)
 		temp = ft_gnl_strjoin(temp);
 	else
 	{
 		temp = (char*)malloc(sizeof(char) * BUFF_SIZE + 1);
 		temp[BUFF_SIZE] = '\0';
+		*i = 0;
 	}
+	return (temp);
+}
+
+char	*handle_newline(char *temp, char **line, int check, int *i)
+{
+	*line = ft_strsub(temp, 0, check);
+	temp = ft_strsub(temp, check + 1, ft_strlen(temp + check));
+	*i = ft_strlen(temp);
+	return (temp);
+}
+
+int		get_next_line(const int fd, char **line)
+{
+	static char	*temp;
+	static int	i;
+	int			check;
+	int			x;
+
+	check = 0;
+	temp = init_temp(temp, &i);
 	if (temp == NULL)
 		return (-1);
 	while (check == 0)
 	{
-		x = read(fd, temp, BUFF_SIZE);
+		x = read(fd, &temp[i], BUFF_SIZE);
+		i += BUFF_SIZE;
 		check = nextline(temp);
-		if (check != 0)
+		if (x == 0)
 		{
-			*line = ft_strsub(temp, 0, check);
-			temp = ft_strsub(temp, check + 1, ft_strlen(temp));
+			while (nextline(temp) != 0)
+				temp = handle_newline(temp, line++, check, &i);
+			*line = temp;
+			return (0);
 		}
+		else if (check != 0)
+			temp = handle_newline(temp, line, check, &i);
 		else
-		{
 			temp = ft_gnl_strjoin(temp);
-		}
 	}
-	return (check);
+	return (1);
 }
--- test.c
+++ test.c
@@ -6,27 +6,35 @@
 /*   By: spoole <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/18 22:28:56 by spoole            #+#    #+#             */
-/*   Updated: 2018/12/19 19:41:17 by spoole           ###   ########.fr       */
+/*   Updated: 2018/12/20 15:03:23 by spoole           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <fcntl.h>
+#include <stdio.h>
 
 int	main(void)
 {
 	int fd = open("./test.txt", O_RDONLY);
 	if (fd < 0)
-		return (1);
+		return (-1);
 	char **result = (char**)malloc(sizeof(char*) * 50);
+	char **search = result;
 	int l = 1;
-	while (l > 0)
+	while (l == 1)
 	{
 		l = get_next_line(fd, result++);
 	}
 	*result = 0;
+	int i = 0;
+	while (*search != NULL)
+	{
+		printf("%d: %s\n", i, *search);
+		search++;
+	}
 	if (l != 0)
-		return (1);
+		return (-1);
 	else
 		return (0);
 }
